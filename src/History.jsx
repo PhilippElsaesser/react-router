67ca70eef5cd27c7b8abd6f45723c5ef0c37880a
Philipp Elsässer <philipp-elsaesser@outlook.com>
2018-10-08 11:17:57 +0200

    added components 'Home' and 'History'

---
+++ src/History.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class History extends Component {
+    render (){
+        return (
+            <h2>Our History</h2>
+        )
+    }
+}
+
+export default History;
